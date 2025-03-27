0e5daa0017741383c35c1a275fe422b7b14e5d7b
Dean <deanfarhan46@gmail.com>
2025-03-27 11:25:39 +0700

    add update  and delete for user, add policies for user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
+    use AuthorizesRequests;
+
+    public function index(Request $request){
+        $this->authorize('viewAny', $request->user);
         return new UserCollection(User::paginate(10));
     }
 
-    public function show(User $user){
+    public function show(Request $request, User $user){
+        $this->authorize('view', $request->user);
         return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request, User $user){
+        $this->authorize('update', $user);
+
+        $user->update($request->all());
+        return response()->json(['message'=>'User successfully updated'], 201);
+    }
+
+    public function destroy(Request $request, User $user){
+        $this->authorize('delete', $request->user);
+        $user->delete();
+        return response()->json([], 204);
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -3,14 +3,71 @@
 namespace App\Policies;
 
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class UserPolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view any models.
      */
-    public function __construct()
+    public function viewAny(User $user): Response
     {
-        //
+        return $user->is_admin
+        ? Response::allow() 
+        : Response::deny('You do not have permission to perform this action');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): Response
+    {
+        return $user->id === $model->id || $user->is_admin
+        ? Response::allow() 
+        : Response::deny('You do not have permission to perform this action');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): Response
+    {
+        return $user->id === $model->id || $user->is_admin
+        ? Response::allow() 
+        : Response::deny('You do not have permission to perform this action');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): Response
+    {
+        return $user->id === $model->id || $user->is_admin
+        ? Response::allow() 
+        : Response::deny('You do not have permission to perform this action');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        return false;
     }
 }
